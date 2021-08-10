a0a4c5e0b726bd5b022b68b1d04f9c74abcd29a4
NayanSidhu <nsidh068@uottawa.ca>
2021-08-10 03:24:59 -0400

    set up sql and tables

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+insert into candidates (first_name, last_name, industry_connected)
+values 
+('Nayan', 'Sidhu', 1),
+('Preeti', 'Sidhu',1),
+('Gagan', 'Gill',0),
+('Sud', 'Batra',0),
+('Shahrukh', 'Bhat', 1);
