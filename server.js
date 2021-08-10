2b0d8c92c349373df837334e4e9ddf48e61bff55
NayanSidhu <nsidh068@uottawa.ca>
2021-08-10 05:21:12 -0400

    linked sql and db

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2'); //Connecting mysql that we installed in node modules to the server
+
 const express = require ('express');
 const PORT = process.env.PORT || 3001; 
 const app = express(); 
@@ -6,7 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connect to the Database
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        //Your MySQL username
+        user:'root',
+        //Your Password
+        password: 'Macbook02!!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
+db.query(`select * from candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other requests (NOT FOUND). ALWAYS make sure this is the last one otherwise it will take presidence over the other GET/POST requests
 app.use((req,res)=>{
